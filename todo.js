646473b20eb871b57408c932c7cdd01ce88577b6
MrLizzaard <dhk21512@gmail.com>
2021-05-23 23:40:11 +0900

    5

--- todo.js
+++ todo.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const lists = document.getElementById("UL").querySelectorAll('li');
+const listlength = lists.length;
+const CB = document.getElementsByClassName("closed");
+
 function ClickAdd() {
   let textVal = document.getElementById("Input");
   if (textVal.value === "") {
@@ -7,10 +11,45 @@ function ClickAdd() {
   } else {
     let ul = document.getElementById("UL");
     let li = document.createElement("li");
+
+    let closebox = document.createElement('span');
+    let txt = document.createTextNode("\u00D7");
+    closebox.className = 'closed';
+    closebox.appendChild(txt);
+    
+
+
     li.appendChild(document.createTextNode(textVal.value));
+    li.appendChild(closebox);
     ul.appendChild(li);
     textVal.value = "";
+    listlength++;
   }
 }
 
-function CheckTheList() {}
+for(let i =0; i<listlength; i++){
+  lists[i].addEventListener('click', function() {
+    if(lists[i].className == ''){
+      lists[i].className = 'checked';
+    }
+    else{
+      lists[i].className = '';
+    }
+  })
+}
+
+for(let i = 0; i<lists.length; i++){
+  let closebox = document.createElement('span');
+  let txt = document.createTextNode("\u00D7");
+  closebox.className = 'closed';
+  closebox.appendChild(txt);
+  lists[i].appendChild(closebox);
+}
+
+
+for(let i = 0; i < CB.length; i++){
+  CB[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
