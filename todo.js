a25ddf3051b0b79aee9b687a07d71c83d0c18e2d
MrLizzaard <dhk21512@gmail.com>
2021-05-25 19:49:09 +0900

    version up

--- todo.js
+++ todo.js
@@ -3,16 +3,41 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-todo");
 const addBtn = document.querySelector(".Btn");
 
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log(toDoInput.value);
+const TODO_LS = [];
+
+function loadToDo(text) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("span");
+  const id = document.querySelectorAll(".js-todo").length + 1;
+  span.className = "text";
+  span.id = id;
+  span.innerText = text;
+  delBtn.className = "del";
+  delBtn.innerText = "❌";
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
 }
 
-function pushBtn(event) {}
+function saveTodo(text, id) {
+  localStorage();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const text = toDoInput.value;
+  loadToDo(text);
+  saveToDo(text);
+
+  toDoInput.value = "";
+}
+
+function pushAddBtn(event) {}
 
 function init() {
   toDoForm.addEventListener("submit", handleSubmit);
-  addBtn.addEventListener("click", pushBtn);
+  addBtn.addEventListener("click", handleSubmit);
 }
 
 init();
