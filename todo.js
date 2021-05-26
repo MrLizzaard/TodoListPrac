63eb9e88f257f38b0cdf420cc4833772328a9d7a
MrLizzaard <dhk21512@gmail.com>
2021-05-26 11:20:08 +0900

    version up

--- todo.js
+++ todo.js
@@ -3,32 +3,48 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-todo");
 const addBtn = document.querySelector(".Btn");
 
+const LS_NAME = "Lists";
 const TODO_LS = [];
+
+function initialize() {
+  let lists = localStorage.getItem(LS_NAME);
+  if (lists !== null) {
+    lists = JSON.parse(lists);
+    lists.forEach((data) => {
+      loadToDo(data.texts);
+    });
+  }
+}
 
 function loadToDo(text) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const delBtn = document.createElement("span");
-  const id = document.querySelectorAll(".js-todo").length + 1;
+  const id = toDoList.querySelectorAll("li").length + 1;
+  const data = {
+    texts: text,
+    toDoId: id,
+  };
+  TODO_LS.push(data);
   span.className = "text";
-  span.id = id;
-  span.innerText = text;
+  span.id = data.toDoId;
+  span.innerText = data.texts;
   delBtn.className = "del";
   delBtn.innerText = "❌";
   li.appendChild(span);
   li.appendChild(delBtn);
   toDoList.appendChild(li);
+  saveTodo();
 }
 
-function saveTodo(text, id) {
-  localStorage();
+function saveTodo() {
+  localStorage.setItem(LS_NAME, JSON.stringify(TODO_LS));
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const text = toDoInput.value;
   loadToDo(text);
-  saveToDo(text);
 
   toDoInput.value = "";
 }
@@ -36,6 +52,7 @@ function handleSubmit(event) {
 function pushAddBtn(event) {}
 
 function init() {
+  initialize();
   toDoForm.addEventListener("submit", handleSubmit);
   addBtn.addEventListener("click", handleSubmit);
 }
