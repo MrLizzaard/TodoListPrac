ebe8fa8f215a01705323c88a192d0096ae575ec4
MrLizzaard <dhk21512@gmail.com>
2021-06-06 00:07:16 +0900

    reset

--- todo.js
+++ todo.js
@@ -1,60 +1,27 @@
-const toDoForm = document.querySelector(".js-form");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-todo");
-const addBtn = document.querySelector(".Btn");
+// const
+const form = document.querySelector(".js-form");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-const LS_NAME = "Lists";
-const TODO_LS = [];
+const TODOS_LS = "lists";
 
-function initialize() {
-  let lists = localStorage.getItem(LS_NAME);
-  if (lists !== null) {
-    lists = JSON.parse(lists);
-    lists.forEach((data) => {
-      loadToDo(data.texts);
-    });
-  }
-}
+// run
 
-function loadToDo(text) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const delBtn = document.createElement("span");
-  const id = toDoList.querySelectorAll("li").length + 1;
-  const data = {
-    texts: text,
-    toDoId: id,
-  };
-  TODO_LS.push(data);
-  span.className = "text";
-  span.id = data.toDoId;
-  span.innerText = data.texts;
-  delBtn.className = "del";
-  delBtn.innerText = "❌";
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  toDoList.appendChild(li);
-  saveTodo();
-}
+form.addEventListener("submit", onSubmit);
 
-function saveTodo() {
-  localStorage.setItem(LS_NAME, JSON.stringify(TODO_LS));
-}
+//function
 
-function handleSubmit(event) {
+function onSubmit(event) {
   event.preventDefault();
-  const text = toDoInput.value;
-  loadToDo(text);
-
-  toDoInput.value = "";
+  let li = returnLi();
+  console.log(li);
 }
 
-function pushAddBtn(event) {}
-
-function init() {
-  initialize();
-  toDoForm.addEventListener("submit", handleSubmit);
-  addBtn.addEventListener("click", handleSubmit);
+function returnLi() {
+  let li = document.createElement("li");
+  let check = document.createElement("i");
+  let trashbin = document.createElement("i");
+  li.innerText = input.value;
+  return li;
 }
-
-init();
